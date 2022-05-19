7987844f0bbc9ace9c54ead0656e277502b22bbe
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-05-19 11:54:29 +0530

    remainder added as remaidner functions

---
+++ remainder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+
+
+int main(){
+    int c=10%3;
+    cout<<c;
+    cout<<"\n";
+    cout<<remainder(10,3.25)<<std::endl;
+    return 0;
+}
