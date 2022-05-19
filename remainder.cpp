3262265f50e97d8e4a502fe5b4ee8c839c8dc6cf
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-05-19 12:04:18 +0530

    remaidner with functions

--- remainder.cpp
+++ remainder.cpp
@@ -3,12 +3,13 @@
 
 using std::cout;
 using std::cin;
-
+using std::endl;
 
 int main(){
+
     int c=10%3;
     cout<<c;
     cout<<"\n";
-    cout<<remainder(10,3.25)<<std::endl;
+    cout<<remainder(10,3.25)<<endl;
     return 0;
 }
