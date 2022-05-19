424959e7f3091f086cb11eb66901155d78838bef
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-05-19 11:30:47 +0530

    bool added

---
+++ bool.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+
+
+int main(){
+    bool is_good=true;
+    cout<<is_good;
+    cout<<"\n";
+    if(is_good){
+        cout<<std::boolalpha<<is_good<<std::endl;
+    }
+}
