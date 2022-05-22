8100a871ef4ed2946d46bd7d8aac94f89e286367
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-05-22 23:01:27 +0530

    getline

--- getline.cpp
+++ getline.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 #include <string>
+
+int main()
+{
+    std::string greeting;
+    getline(std::cin, greeting);
+    greeting.append(" Karthik");
+    std::string name = "karthik k";
+    std::string newname=name.insert(7, " sunil");
+
+    //std::cout << greeting<<std::endl;
+    std::cout << newname;
+    std::cout<<newname.erase(7,8)<<std::endl;
+    std::cout<<newname;
+
+    return 0;
+}
