b061b490deed508bd9846f94aa4a31277994c0b0
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-05-23 23:09:36 +0530

    getline modified

--- getline.cpp
+++ getline.cpp
@@ -8,7 +8,6 @@ int main()
     greeting.append(" Karthik");
     std::string name = "karthik k";
     std::string newname = name.insert(7, " sunil");
-
     std::cout << greeting << std::endl;
     std::cout << newname << std::endl;
     std::cout << newname.erase(7, 8) << std::endl;
@@ -16,5 +15,14 @@ int main()
     std::cout << newname << std::endl;
     newname.replace(0, 5, "lakshm");
     std::cout << newname << std::endl;
+    std::cout << "\n";
+    std::cout << greeting.find("xxx") << std::endl;
+    // it returns 0 means the word exist and it return a huge number means it
+    // does not exist and the huge number is unsignedlong int of 1
+
+    unsigned long x = -1;
+    std::cout << x;
+    // does not exist and the huge number is unsignedlong int of 1
+
     return 0;
 }
