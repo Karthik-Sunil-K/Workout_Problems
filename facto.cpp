652d2feb2c8a01a8b5c4063a6895813894855c5f
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-01 22:59:20 +0530

    do while added

---
+++ facto.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <float.h>
+using std::cout;
+using std::cin;
+
+
+int main(){
+   int i;
+   int k=1;
+   for ( i = 1; i <= 5; i++)
+   {
+       k=k*i;
+   }
+   std::cout<<k;
+   
+    return 0;
+}
