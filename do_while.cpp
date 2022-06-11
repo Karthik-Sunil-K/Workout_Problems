1337167383efe8dc7587f29e8c70d77949bab4a5
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-11 20:05:34 +0530

    added menu

---
+++ do_while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+int main(){
+
+    string password1="hello";
+    string password2;
+    do
+    {
+        cout<<"enter password";
+        cin>>password2;
+    } while (false);
+    return 0;
+
+}
