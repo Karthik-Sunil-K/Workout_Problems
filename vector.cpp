c3fcc3466bf4730c745c356f102167bf2017ff1b
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-21 23:05:41 +0530

    swap

--- vector.cpp
+++ vector.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
-using std::cout;
-using std::cin;
-using std::endl;
+// using std::cout;
+// using std::cin;
+// using std::endl;
+using namespace std;
+
+
 
 int main(){
-   std::vector<int> items={1,2} ;
-   items.push_back(4);
-   for (int i = 0; i < items.size(); i++)
+   vector<int> items={1,2} ;
+   vector<int> items1={3,4};
+  for(int i=0;i<2;i++){
+      swap(items[i],items1[i]);
+   }
+   for (int i = 0; i < 2; i++)
    {
-    cout<<items.back()<<endl;
+     cout<<items[i];
    }
    
+
+   
 }
+
+
+
